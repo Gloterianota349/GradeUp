697106cdbbc2f021d01e186f4a8f428792bf9782
PedroEmanoel933 <pemanoel067@gmail.com>
2025-07-22 11:54:10 -0300

    feat: Estrutura da página de login

---
+++ View/login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login | GradeUp</title>
+    <link rel="stylesheet" href="../templates/login.css">
+    <link rel="icon" href="../View/img/A+.png">
+</head>
+<body>
+    <main>
+        <div class="login_box">
+            
+            <div class="logo_storytelling">
+                <figure class="logo_a">
+                    <img src="../View/img/A+.png" alt="A+ vermelho">
+                </figure>
+                
+                <figure class="logo_gradeup">
+                    <img src="../View/img/logo.png" alt="Logo GradeUp">
+                </figure>
+                <h1>Bem vindo novamente</h1>
+            </div>
+            
+                <div class="form_box">
+                    <form>
+                        <label for="userEmail">
+                            <p>Email</p>
+                            <input type="email" name="userEmail" id="userEmail" placeholder="Digite o seu E-mail">
+                        </label>
+                        
+                        <label for="password">
+                            <p>Senha</p>
+                            <input type="email" name="password" id="password" placeholder="Digite sua senha">
+                        </label>
+                    </form>
+                </div>
+                    
+                <button type="submit">Entrar</button>
+            
+            
+            <p class="inscreva-se">Ainda não tem uma conta? <span>Inscreva-se</span></p>
+        </div>
+    </main>
+</body>
+</html>
