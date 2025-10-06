a254517c2b5a1f3d1b3d347e512bd09d3ec6ceeb
biamot4 <beatrizmotasesi@gmail.com>
2025-10-06 01:12:43 -0300

    feat: Testes completos do back-end do Login

--- tests/LoginTest.php
+++ tests/LoginTest.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once __DIR__ . '/../vendor/autoload.php';
 use PHPUnit\Framework\TestCase;
 
 use Controller\UserController;
@@ -7,19 +8,20 @@ use Controller\UserController;
 use Model\User;
 
 class LoginTest extends TestCase{
-    private $usercontroller;
+    private $userController;
 
     private $mockUserModel;
 
-    public function setUp(): void{
-            $this -> mockUserModel = $this -> createMock(User::class);
-        
-            $this -> usercontroller = new UserController($this -> mockUserModel);
+    public function setUp(): void {
+        $_SESSION = [];
+        $this->mockUserModel = $this->createMock(User::class);
+        $this->userController = new UserController($this->mockUserModel);
     }
+
 
     #[\PHPUnit\Framework\Attributes\Test]
     public function it_should_be_able_to_sign_in(){
-        $this -> mockUserModel -> method('registerUser') -> willReturn(true) ([
+        $this -> mockUserModel -> method('getUser_Email') -> willReturn([
             'id' => 1,
             'user_fullname' => 'Pedro Emanoel',
             'email' => 'pedro@example.com',
@@ -29,7 +31,6 @@ class LoginTest extends TestCase{
         $userResult = $this -> userController -> login('pedro@example.com', '123456');
         
         $this->assertTrue($userResult);
-
         $this -> assertEquals(1, $_SESSION['id']);
         $this -> assertEquals('Pedro Emanoel', $_SESSION['user_fullname']);
         $this -> assertEquals('pedro@example.com', $_SESSION['email']);
@@ -37,7 +38,7 @@ class LoginTest extends TestCase{
 
     #[\PHPUnit\Framework\Attributes\Test]
     public function it_shouldnt_login_with_invalid_credentials(){
-        $this -> mockUserModel -> method('registerUser') -> willReturn(true) ([
+        $this -> mockUserModel -> method('getUser_Email') -> willReturn ([
             'id' => 1,
             'user_fullname' => 'Pedro Emanoel',
             'email' => 'pedro@example.com',
@@ -52,9 +53,11 @@ class LoginTest extends TestCase{
     #[\PHPUnit\Framework\Attributes\Test]
     public function It_should_verify_if_is_logged_in(){
         $_SESSION['id'] = 1;
-        $userResult = $this -> usercontroller -> isLoggedIn();
+        $userResult = $this -> userController -> isLoggedIn();
 
         $this -> assertTrue($userResult);
     }
 }
+
 ?>
+
